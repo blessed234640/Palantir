c4afec3a3442d46736141744bc4097a3f6e45bf1
blessed234640 <Arturazimov200577@gmail.com>
2026-07-05 16:06:14 +0600

    add schemas

--- app/schemas.py
+++ app/schemas.py
@@ -3,13 +3,13 @@ from typing import Literal
 
 
 class Message(BaseModel):
-    model: str = Field(description="название модели которую мы вызываем")
-    role: str = Field(Literal["system", "user", "assistant", "tool"], description="роль отправителя")
+    role: Literal["system", "user", "assistant", "tool"] = Field(description="роль отправителя")
     content: str = Field(description="отправленный контент")
 
     
 class ChatCompletionRequest(BaseModel):
+    model: str = Field(description="название модели которую мы вызываем")
     messages: list[Message]
-    stream: bool | None = Field(description="ответ по кусочкам или отдать целиком")
-    temperature: float | None =  Field(description="креативность")
-    max_tokens: int | None = Field(description="потолок длины ответа")
+    stream: bool = Field(default=False, description="ответ по кусочкам или отдать целиком")
+    temperature: float | None =  Field(default=None, description="креативность")
+    max_tokens: int | None = Field(default=None, description="потолок длины ответа")
